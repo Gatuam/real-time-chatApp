f7f2a0269829637664c7221312f4e1b0a3000016
EK Gautham <sggtm42@gmail.com>
2025-06-29 21:49:46 +0700

    init backend

---
+++ server/src/controllers/auth.controllers.js
@@ -0,0 +1,10 @@
+
+export const signup = async (req, res) => {
+    res.send('hello')
+}
+export const login = async (req, res) => {
+    res.send('hello')
+}
+export const logout = async (req, res) => {
+    res.send('hello')
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,12 @@
 import express, { json, urlencoded } from 'express';
 import env from 'dotenv';
+import authRoutes from './routes/auth.routes.js'
 env.config();
 const app = express();
 app.use(json());
 app.use(urlencoded({extended: true}));
 
-app.get('/', (req, res)=>{
-    res.send("hello");
-})
-
+app.use('/app/auth', authRoutes);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`app is runng on ${process.env.PORT}`)
