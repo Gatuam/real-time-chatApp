7b17c86554fa095bf5de365499138ad7aba60454
Ek Gautham <sggtm42@gmail.com>
2025-07-08 12:42:32 +0700

    init client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Setting from './pages/Setting'
+import Profile from './pages/Profile'
 
-
-function App() {
-
+const App = () => {
   return (
     <div>
-      hi
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/' element={<Signup/>} />
+          <Route path='/' element={<Login/>} />
+          <Route path='/' element={<Setting/>} />
+          <Route path='/' element={<Profile/>} />
+        </Routes>
+      </Navbar>
     </div>
   )
 }
