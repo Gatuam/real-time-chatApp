d290ade21e1542ed9009d437deee577d4d9204b9
EK GAUTHAM <sggtm42@gmail.com>
2025-09-26 16:50:46 +0700

    cookie deployment error

--- server/src/utils/util.js
+++ server/src/utils/util.js
@@ -1,14 +1,14 @@
-import JWT from "jsonwebtoken";
-
 export const genToken = async (userId, res) => {
   const token = JWT.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
+
   res.cookie("token", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    secure: process.env.NODE_ENV === "development" ? false : true,
   });
+
   return token;
 };
