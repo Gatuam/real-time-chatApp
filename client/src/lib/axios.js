fe7cf16a80d6f026169f149a75cbf7db362aeaad
EK GAUTHAM <sggtm42@gmail.com>
2025-09-26 13:41:13 +0700

    url

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3001/api"
-      : "/api",
+      : "https://real-time-chatapp-fd97.onrender.com/api",
   withCredentials: true,
 });
